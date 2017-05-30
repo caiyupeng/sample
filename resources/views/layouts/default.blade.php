165948feb435cd8d60204b63a3514374134f884b
caiyupeng <350166681@qq.com>
2017-05-30 01:42:34 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title', 'Sample') - Laravel 入门教程</title>
+  </head>
+  <body>
+    <!-- 表示该占位区域将用于显示 content 区块的内容 -->
+    @yield('content')
+  </body>
+</html>
